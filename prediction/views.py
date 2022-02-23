3514fa973af3c3a4d3b1a9aa2e403fbcf12218f0
zcj0215 <3261524748@qq.com>
2022-02-24 05:53:19 +0800

    add prediction and update setting.py

---
+++ prediction/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from .lstm_prediction import *
+
+# --------------- MAIN WEB PAGES -----------------------------
+def redirect_root(request):
+    return redirect('/pred_app/index')
+
+def index(request):
+	return render(request, 'pred_app/index.html') 
+
+def pred(request):
+    return render(request, 'pred_app/prediction.html')
+
+def contact(request):
+	return render(request, 'pred_app/contact.html')
+
+def search(request, se, stock_symbol):
+	import json
+	predicted_result_df = lstm_prediction(se, stock_symbol)
+	return render(request, 'pred_app/search.html', {"predicted_result_df": predicted_result_df})
+# -----------------------------------------------------------
+
+
+
+
+
+
+
+""" from django.shortcuts import render
+
+# Create your views here.
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np 
+import math
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+
+
+
+# 将一维数据创建为时间序列数据集的功能
+def new_dataset(dataset, step_size):
+	data_X, data_Y = [], []
+	for i in range(len(dataset)-step_size-1):
+		a = dataset[i:(i+step_size), 0]
+		data_X.append(a)
+		data_Y.append(dataset[i + step_size, 0])
+	return np.array(data_X), np.array(data_Y)
+
+# 此函数可用于从任何一维数组创建时间序列数据集	
+
+def predict():
+    # 可重复性
+    np.random.seed(7) 
+    # IMPORTING DATASET 
+    dataset = pd.read_csv('apple_share_price.csv', usecols=[1,2,3,4])
+    dataset = dataset.reindex(index = dataset.index[::-1]) """
--- teststock/settings.py
+++ teststock/settings.py
@@ -177,8 +177,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
