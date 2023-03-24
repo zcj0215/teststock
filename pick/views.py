f56e129066b89ef6112668f40d7903235356b255
zcj0215 <3261524748@qq.com>
2023-03-24 08:26:49 +0800

    '601519'

--- pick/views.py
+++ pick/views.py
@@ -382,7 +382,7 @@ def query(request):
 
 
     for data in data_list:
-        turnover('002195','SZ',data[1], data[0])
+        turnover('601519','SH',data[1], data[0])
 
     return HttpResponse('执行完毕！')
 
