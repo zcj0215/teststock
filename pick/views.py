ee55e64677daf6fae7e0403ff35eec6a838847d2
zcj0215 <3261524748@qq.com>
2023-06-08 06:07:06 +0800

    '每日数据2023-06-07'

--- pick/views.py
+++ pick/views.py
@@ -472,7 +472,7 @@ def dayadd(request):
                 data[6] = round(float(data[6][0:-1])*10000,2)
         
         print(data)
-        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-06-06')
+        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-06-07')
 
     return HttpResponse('执行完毕！')
 
