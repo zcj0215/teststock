6110b9cc360fd63da6137b98852c3f829d4aedc3
zcj0215 <3261524748@qq.com>
2023-10-19 04:36:58 +0800

    '算力概念板块'

--- pick/views.py
+++ pick/views.py
@@ -484,10 +484,10 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\证券.xls"
+        filename = path+"\\算力概念.xls"
         
     else:
-        filename = path+"/证券.xls"
+        filename = path+"/算力概念.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -506,11 +506,11 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='证券')  
+        board = get_object_or_404(Board,name='算力概念')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='证券'):
+            if  not stocks.boards.filter(name='算力概念'):
                 stocks.boards.add(board)
                 stocks.save()
                     
