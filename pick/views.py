4fd2bb322922c1c7dd095765134a202a98c8d6f3
zcj0215 <3261524748@qq.com>
2023-10-21 05:50:56 +0800

    '锂电池概念'

--- pick/views.py
+++ pick/views.py
@@ -484,10 +484,10 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\风电.xls"
+        filename = path+"\\锂电池概念.xls"
         
     else:
-        filename = path+"/风电.xls"
+        filename = path+"/锂电池概念.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -506,11 +506,11 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='风电')  
+        board = get_object_or_404(Board,name='锂电池概念')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='风电'):
+            if  not stocks.boards.filter(name='锂电池概念'):
                 stocks.boards.add(board)
                 stocks.save()
                     
