ad9d50d1524134c86a930309fc140b664c5bef71
zcj0215 <3261524748@qq.com>
2023-10-29 19:57:41 +0800

    '2023-10-27'

--- pick/views.py
+++ pick/views.py
@@ -519,7 +519,7 @@ def dayadd(request):
                 data[6] = round(float(data[6][0:-1])*10000,2)
         
         print(data)
-        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-10-26')
+        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-10-27')
 
     return HttpResponse('执行完毕！')
 
@@ -628,7 +628,7 @@ def blockdayadd(request):
         except:
             pass
             
-        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2023-10-26')
+        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2023-10-27')
 
     return HttpResponse('执行完毕！')
 
