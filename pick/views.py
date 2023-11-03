f5237ab3124def824b6e22359fda24b811dbb6f9
zcj0215 <3261524748@qq.com>
2023-11-04 04:35:17 +0800

    '通用机械'

--- pick/views.py
+++ pick/views.py
@@ -545,7 +545,7 @@ def dayadd(request):
                 data[6] = round(float(data[6][0:-1])*10000,2)
         
         print(data)
-        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-11-02')
+        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-11-03')
 
     return HttpResponse('执行完毕！')
 
@@ -554,10 +554,10 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\行业龙头.xls"
+        filename = path+"\\通用机械.xls"
         
     else:
-        filename = path+"/行业龙头.xls"
+        filename = path+"/通用机械.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -576,11 +576,11 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='行业龙头')  
+        board = get_object_or_404(Board,name='通用机械')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='行业龙头'):
+            if  not stocks.boards.filter(name='通用机械'):
                 stocks.boards.add(board)
                 stocks.save()
                     
@@ -661,7 +661,7 @@ def blockdayadd(request):
         except:
             pass
             
-        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2023-11-02')
+        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2023-11-03')
 
     return HttpResponse('执行完毕！')
 
