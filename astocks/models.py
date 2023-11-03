a99ea088b5ef787a943641564aea3d7a98231695
zcj0215 <3261524748@qq.com>
2023-11-04 06:43:22 +0800

    '元器件'

--- astocks/models.py
+++ astocks/models.py
@@ -73,6 +73,7 @@ class Stocks(models.Model):
     name = models.CharField(max_length=50)
     boards = models.ManyToManyField(Board, related_name="stockses")
     growth=models.DecimalField(max_digits=10, decimal_places=2,null=True)
+    blockname=models.CharField(max_length=50,null=True)
 
     def __str__(self):
         return self.name
