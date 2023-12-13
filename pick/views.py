6375069f0216c123cfe5675fbd455e0a8c5a72ce
zcj0215 <3261524748@qq.com>
2023-12-14 04:32:03 +0800

    '20231213'

--- pick/views.py
+++ pick/views.py
@@ -599,9 +599,9 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\最近异动.xls"       
+        filename = path+"\\供气供热.xls"       
     else:
-        filename = path+"/最近异动.xls"
+        filename = path+"/供气供热.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -620,23 +620,23 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='最近异动')  
+        board = get_object_or_404(Board,name='供气供热')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='最近异动'):
+            if  not stocks.boards.filter(name='供气供热'):
                 stocks.boards.add(board)
-                stocks.blockname = '最近异动'
+                stocks.blockname = '供气供热'
                 stocks.save()
-            elif stocks.boards.filter(name='最近异动'):
-                stocks.blockname = '最近异动'
+            elif stocks.boards.filter(name='供气供热'):
+                stocks.blockname = '供气供热'
                 stocks.save()
                     
         except Http404:
             stocks = Stocks.objects.create(
                 code = my,
                 name = row.名称,
-                blockname = '最近异动'
+                blockname = '供气供热'
             )
             stocks.boards.add(board)
             stocks.save()     
