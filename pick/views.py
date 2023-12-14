c6989863200af835ffd753746a20021d23816fc8
zcj0215 <3261524748@qq.com>
2023-12-15 04:49:35 +0800

    '20231215xxx'

--- pick/views.py
+++ pick/views.py
@@ -590,7 +590,7 @@ def dayadd(request):
                 data[6] = round(float(data[6][0:-1])*10000,2)
         
         print(data)
-        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-12-12')
+        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2023-12-14')
 
     return HttpResponse('执行完毕！')
 
@@ -599,9 +599,9 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\供气供热.xls"       
+        filename = path+"\\上海板块.xls"       
     else:
-        filename = path+"/供气供热.xls"
+        filename = path+"/上海板块.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -620,23 +620,23 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='供气供热')  
+        board = get_object_or_404(Board,name='上海板块')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='供气供热'):
+            if  not stocks.boards.filter(name='上海板块'):
                 stocks.boards.add(board)
-                stocks.blockname = '供气供热'
+                stocks.blockname = '上海板块'
                 stocks.save()
-            elif stocks.boards.filter(name='供气供热'):
-                stocks.blockname = '供气供热'
+            elif stocks.boards.filter(name='上海板块'):
+                stocks.blockname = '上海板块'
                 stocks.save()
                     
         except Http404:
             stocks = Stocks.objects.create(
                 code = my,
                 name = row.名称,
-                blockname = '供气供热'
+                blockname = '上海板块'
             )
             stocks.boards.add(board)
             stocks.save()     
@@ -710,7 +710,7 @@ def blockdayadd(request):
         except:
             pass
             
-        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2023-12-12')
+        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2023-12-14')
 
     return HttpResponse('执行完毕！')
 
@@ -755,7 +755,7 @@ def inflow(request):
             except: 
                inf = 0
             
-        everyday_inflow(code, inf, '2023-12-12')
+        everyday_inflow(code, inf, '2023-12-14')
     
     return HttpResponse('执行完毕！')
 
@@ -796,15 +796,13 @@ def inflow_single(request):
         
         return render(request, 'inflow.html', {'form': form})
     
-    
-    
 def stock_single(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\Table.xls"       
+        filename = path+"\\Table1.xls"       
     else:
-        filename = path+"/Table.xls"
+        filename = path+"/Table1.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     for row in df.itertuples():
@@ -812,14 +810,8 @@ def stock_single(request):
         print(dt)
         amount = round((row.金额)/10000,2)
         
-        turnover('300612','SZ',row.换手, amount, dt)
+        turnover('300031','SZ',row.换手, amount, dt)
         
         
     
     return HttpResponse('执行完毕！')
-    
-    
-    
-    
-    
-    
