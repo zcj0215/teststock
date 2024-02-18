ec2425ba74065c2fabdc442d07a9589c66b1f365
zcj0215 <3261524748@qq.com>
2024-02-18 20:32:29 +0800

    '20240208_18_3'

--- pick/views.py
+++ pick/views.py
@@ -610,9 +610,9 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\镍金属.xls"
+        filename = path+"\\MiniLED.xls"
     else:
-        filename = path+"/镍金属.xls"
+        filename = path+"/MiniLED.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -631,23 +631,23 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='镍金属')  
+        board = get_object_or_404(Board,name='MiniLED')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='镍金属'):
+            if  not stocks.boards.filter(name='MiniLED'):
                 stocks.boards.add(board)
-                stocks.blockname = '镍金属'
+                stocks.blockname = 'MiniLED'
                 stocks.save()
-            elif stocks.boards.filter(name='镍金属'):
-                stocks.blockname = '镍金属'
+            elif stocks.boards.filter(name='MiniLED'):
+                stocks.blockname = 'MiniLED'
                 stocks.save()
                     
         except Http404:
             stocks = Stocks.objects.create(
                 code = my,
                 name = row.名称,
-                blockname = '镍金属'
+                blockname = 'MiniLED'
             )
             stocks.boards.add(board)
             stocks.save() 
