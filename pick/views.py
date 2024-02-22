0a41f63eac85b3bc5b5b51fca9c1ac1744bb86f0
zcj0215 <3261524748@qq.com>
2024-02-23 04:55:27 +0800

    '20240222_1'

--- pick/views.py
+++ pick/views.py
@@ -648,9 +648,9 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\NMN概念.xls"
+        filename = path+"\\新型烟草.xls"
     else:
-        filename = path+"/NMN概念.xls"
+        filename = path+"/新型烟草.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -669,23 +669,23 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='NMN概念')  
+        board = get_object_or_404(Board,name='新型烟草')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='NMN概念'):
+            if  not stocks.boards.filter(name='新型烟草'):
                 stocks.boards.add(board)
-                stocks.blockname = 'NMN概念'
+                stocks.blockname = '新型烟草'
                 stocks.save()
-            elif stocks.boards.filter(name='NMN概念'):
-                stocks.blockname = 'NMN概念'
+            elif stocks.boards.filter(name='新型烟草'):
+                stocks.blockname = '新型烟草'
                 stocks.save()
                     
         except Http404:
             stocks = Stocks.objects.create(
                 code = my,
                 name = row.名称,
-                blockname = 'NMN概念'
+                blockname = '新型烟草'
             )
             stocks.boards.add(board)
             stocks.save() 
