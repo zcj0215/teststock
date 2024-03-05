a2d0b98c83b39547dd7aec2939aa5ecb76786547
zcj0215 <3261524748@qq.com>
2024-03-05 23:49:20 +0800

    '20240305_5'

--- pick/views.py
+++ pick/views.py
@@ -904,6 +904,6 @@ def stock_single(request):
         print(row.金额)
         amount = round((row.金额)/10000,2)
         
-        turnover('300638','SZ',row.换手, amount, dt)
+        turnover('002236','SZ',row.换手, amount, dt)
         
     return HttpResponse('执行完毕！')
