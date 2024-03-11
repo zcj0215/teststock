6a4a11564bf192017134654682816c94af058792
zcj0215 <3261524748@qq.com>
2024-03-12 05:39:59 +0800

    '20240311'

--- pick/views.py
+++ pick/views.py
@@ -639,7 +639,7 @@ def dayadd(request):
                 data[6] = round(float(data[6][0:-1])*10000,2)
         
         print(data)
-        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2024-03-08')
+        everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],'2024-03-11')
 
     return HttpResponse('执行完毕！')
 
@@ -648,9 +648,9 @@ def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
     if(sysstr =="Windows"):
-        filename = path+"\\小米概念.xls"
+        filename = path+"\\超导概念.xls"
     else:
-        filename = path+"/小米概念.xls"
+        filename = path+"/超导概念.xls"
         
     df = pd.read_excel(filename, sheet_name='工作表1', header=0)
     
@@ -669,23 +669,23 @@ def blockadd(request):
             my = '0'+ my    
         print(my)
         print(row.名称)
-        board = get_object_or_404(Board,name='小米概念')  
+        board = get_object_or_404(Board,name='超导概念')  
         try:
             stocks = get_object_or_404(Stocks,code=my)
             
-            if  not stocks.boards.filter(name='小米概念'):
+            if  not stocks.boards.filter(name='超导概念'):
                 stocks.boards.add(board)
-                stocks.blockname = '小米概念'
+                stocks.blockname = '超导概念'
                 stocks.save()
-            elif stocks.boards.filter(name='小米概念'):
-                stocks.blockname = '小米概念'
+            elif stocks.boards.filter(name='超导概念'):
+                stocks.blockname = '超导概念'
                 stocks.save()
                     
         except Http404:
             stocks = Stocks.objects.create(
                 code = my,
                 name = row.名称,
-                blockname = '小米概念'
+                blockname = '超导概念'
             )
             stocks.boards.add(board)
             stocks.save() 
@@ -759,7 +759,7 @@ def blockdayadd(request):
         except:
             pass
             
-        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2024-03-08')
+        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_20,growth_60,Continuerise_days,'2024-03-11')
 
     return HttpResponse('执行完毕！')
 
@@ -804,7 +804,7 @@ def inflow(request):
             except: 
                inf = 0
             
-        everyday_inflow0(code, inf, '2024-03-08')
+        everyday_inflow0(code, inf, '2024-03-11')
     
     return HttpResponse('执行完毕！')
 
