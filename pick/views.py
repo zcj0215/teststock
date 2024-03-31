452879347170170f6bf09286e4dbbdc616c9c9c3
zcj0215 <3261524748@qq.com>
2024-03-31 18:22:43 +0800

    '20240329_4'

--- pick/views.py
+++ pick/views.py
@@ -984,8 +984,8 @@ def index_single(request):
 def block_single(request):
     path =  os.path.dirname(__file__)
     filename = ""
-    code = "880701"
-    name ="Sora概念"
+    code = "880933"
+    name ="智能医疗"
     if(sysstr =="Windows"):
         filename = path+"\\"+code+".csv"       
     else:
