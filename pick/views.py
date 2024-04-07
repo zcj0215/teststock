07da7351d59d4629a6f305d28a3b084e9d6345de
zcj0215 <3261524748@qq.com>
2024-04-08 02:46:00 +0800

    '20240403_21'

--- pick/views.py
+++ pick/views.py
@@ -934,8 +934,8 @@ def index_single(request):
 def block_single(request):
     path =  os.path.dirname(__file__)
     filename = ""
-    code = "880490"
-    name ="通信设备"
+    code = "880452"
+    name ="电信运营"
     if(sysstr =="Windows"):
         filename = path+"\\"+code+".csv"       
     else:
