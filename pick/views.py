eccd503490844e4dbca626c3b713c1fb296f156c
zcj0215 <3261524748@qq.com>
2024-04-14 08:10:14 +0800

    '20240412_4'

--- pick/views.py
+++ pick/views.py
@@ -1064,8 +1064,8 @@ def index_single(request):
 def block_single(request):
     path =  os.path.dirname(__file__)
     filename = ""
-    code = "880472"
-    name ="证券"
+    code = "880367"
+    name ="纺织服饰"
     if(sysstr =="Windows"):
         filename = path+"\\"+code+".csv"       
     else:
@@ -1086,7 +1086,7 @@ def block_single(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="综合类"
+    blockname ="纺织服饰"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
