96974cc0ccca43a2ecc890465bf22e90a30668e3
zcj0215 <3261524748@qq.com>
2024-04-28 06:53:50 +0800

    '20240426_3'

--- pick/views.py
+++ pick/views.py
@@ -1148,8 +1148,8 @@ def index_single(request):
 def block_single(request):
     path =  os.path.dirname(__file__)
     filename = ""
-    code = "880802"
-    name ="QFII重仓"
+    code = "880806"
+    name ="社保重仓"
     if(sysstr =="Windows"):
         filename = path+"\\"+code+".csv"       
     else:
@@ -1170,7 +1170,7 @@ def block_single(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="QFII重仓"
+    blockname ="社保重仓"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
