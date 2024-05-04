72324b4734e88f950a083b276c6a60e674dcda54
zcj0215 <3261524748@qq.com>
2024-05-04 16:37:58 +0800

    '20240430_30'

--- pick/views.py
+++ pick/views.py
@@ -1229,8 +1229,8 @@ def index_single(request):
 def block_single(request):
     path =  os.path.dirname(__file__)
     filename = ""
-    code = "880696"
-    name ="小米汽车"
+    code = "880660"
+    name ="工业软件"
     if(sysstr =="Windows"):
         filename = path+"\\"+code+".csv"       
     else:
@@ -1251,7 +1251,7 @@ def block_single(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="小米汽车"
+    blockname ="工业软件"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
