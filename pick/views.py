a7e36ab23d6330f5267240bbc61702bccdc4bda0
zcj0215 <3261524748@qq.com>
2024-05-09 20:35:48 +0800

    '20240509'

--- pick/views.py
+++ pick/views.py
@@ -808,7 +808,7 @@ def dayadd(request):
             
             data = [code,row.开盘,row.最新,row.最高,row.最低,volume,amount,row.换手,row.量比,row.涨幅,row.涨跌,row.昨收,row.市盈率,row.委比]    
             print(data)
-            everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],'2024-05-08')
+            everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],'2024-05-09')
 
     return HttpResponse('执行完毕！')
 
@@ -830,8 +830,7 @@ def indexadd(request):
             volume = round(float(row.总手*1/1000000),2)
             amount = round(float(row.金额/100000000),2)
         
-        
-            everyday_index(code,row.名称, row.开盘, row.现价, row.最高, row.最低, volume, amount, row.涨跌,  row.涨幅, row.振幅, '2024-05-08')
+            everyday_index(code,row.名称, row.开盘, row.现价, row.最高, row.最低, volume, amount, row.涨跌,  row.涨幅, row.振幅, '2024-05-09')
         
     return HttpResponse('执行完毕！')    
 
@@ -861,7 +860,7 @@ def indexpe(request):
         print(code)
         print(row.市盈率)
         
-        everyday_indexpe(code,row.市盈率, '2024-05-08')
+        everyday_indexpe(code,row.市盈率, '2024-05-09')
             
     
     return HttpResponse('执行完毕！') 
@@ -942,7 +941,7 @@ def blockdayadd(request):
         
         
             
-        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_fall,Continuerise_30_limitup,Continuerise_days,row.市盈率,'2024-05-08')
+        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_fall,Continuerise_30_limitup,Continuerise_days,row.市盈率,'2024-05-09')
 
     return HttpResponse('执行完毕！')
 
@@ -987,7 +986,7 @@ def inflow(request):
             except: 
                inf = 0
             
-        everyday_inflow0(code, inf, '2024-05-08')
+        everyday_inflow0(code, inf, '2024-05-09')
     
     return HttpResponse('执行完毕！')
 
@@ -1032,7 +1031,7 @@ def nf(request):
             except: 
                inf = 0
         print(inf)
-        everyday_nf(code, inf, '2024-05-08')
+        everyday_nf(code, inf, '2024-05-09')
     
     return HttpResponse('执行完毕！')
 
@@ -1232,8 +1231,8 @@ def index_single(request):
 def block_single(request):
     path =  os.path.dirname(__file__)
     filename = ""
-    code = "880758"
-    name ="汽车芯片"
+    code = "880973"
+    name ="医美概念"
     if(sysstr =="Windows"):
         filename = path+"\\"+code+".csv"       
     else:
@@ -1254,7 +1253,7 @@ def block_single(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="汽车芯片"
+    blockname ="医美概念"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
