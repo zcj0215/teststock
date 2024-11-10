b7af975792e9b58bcf473b4e2b0aae957d2b9631
赵从军 <702065725@qq.com>
2024-11-10 22:33:33 +0800

    '20240620'

--- pick/views.py
+++ pick/views.py
@@ -808,7 +808,7 @@ def dayadd(request):
             
             data = [code,row.开盘,row.最新,row.最高,row.最低,volume,amount,row.换手,row.量比,row.涨幅,row.涨跌,row.昨收,row.市盈率,row.委比]    
             print(data)
-            everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],'2024-06-19')
+            everyday(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],'2024-06-20')
 
     return HttpResponse('执行完毕！')
 
@@ -830,7 +830,7 @@ def indexadd(request):
             volume = round(float(row.总手*1/1000000),2)
             amount = round(float(row.金额/100000000),2)
         
-            everyday_index(code,row.名称, row.开盘, row.现价, row.最高, row.最低, volume, amount, row.涨跌,  row.涨幅, row.振幅, '2024-06-19')
+            everyday_index(code,row.名称, row.开盘, row.现价, row.最高, row.最低, volume, amount, row.涨跌,  row.涨幅, row.振幅, '2024-06-20')
         
     return HttpResponse('执行完毕！')    
 
@@ -860,7 +860,7 @@ def indexpe(request):
         print(code)
         print(row.市盈率)
         
-        everyday_indexpe(code,row.市盈率, '2024-06-19')
+        everyday_indexpe(code,row.市盈率, '2024-06-20')
             
     
     return HttpResponse('执行完毕！') 
@@ -941,7 +941,7 @@ def blockdayadd(request):
         
         
             
-        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_fall,Continuerise_30_limitup,Continuerise_days,row.市盈率,'2024-06-19')
+        everyday_block(row.代码,row.名称,row.今开,row.现价,row.最高,row.最低,row.总量,turnover,row.量比,row.昨收,limitup_number,row.涨幅,growth_pre,growth_3,growth_fall,Continuerise_30_limitup,Continuerise_days,row.市盈率,'2024-06-20')
 
     return HttpResponse('执行完毕！')
 
@@ -986,7 +986,7 @@ def inflow(request):
             except: 
                inf = 0
             
-        everyday_inflow0(code, inf, '2024-06-19')
+        everyday_inflow0(code, inf, '2024-06-20')
     
     return HttpResponse('执行完毕！')
 
