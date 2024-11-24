657790a981ffdabee193fa4dfe3e49a5effdccb7
赵从军 <702065725@qq.com>
2024-11-25 05:20:28 +0800

    '603666'

--- pick/views.py
+++ pick/views.py
@@ -1301,7 +1301,7 @@ def stock_single(request):
         print(row.金额)
         amount = round((row.金额)/10000,2)
         
-        turnover('605111','SH',row.换手, amount, dt)
+        turnover('603666','SH',row.换手, amount, dt)
         
     return HttpResponse('执行完毕！')
 
