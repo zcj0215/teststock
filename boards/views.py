94ac377e2d2ff9c9e97860921c2af992d55edd64
赵从军 <702065725@qq.com>
2025-01-24 11:21:06 +0800

    '20250123_2'

--- boards/views.py
+++ boards/views.py
@@ -125,7 +125,7 @@ class StockListView(ListView):
             self.data = json.dumps(blocklist)
             self.bsignals = bsignals.to_json()
         
-        queryset = Stocks.objects.filter(boards__name__in = [self.board.name]).order_by('-growth')
+        queryset = Stocks.objects.filter(boards__name__in = [self.board.name]).order_by('-capital_inflow')
         return queryset
 
 class PostListView(ListView):
