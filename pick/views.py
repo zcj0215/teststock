a70bef6f906c1d6fe2c298e59ba29a6985e4adbb
赵从军 <702065725@qq.com>
2025-01-31 18:23:03 +0800

    '20241203_13'

--- pick/views.py
+++ pick/views.py
@@ -1555,7 +1555,7 @@ def block_single(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="光伏"
+    blockname ="固态电池"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
