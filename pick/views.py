4bc03de874311cc45da150089886a4046cce3b19
赵从军 <702065725@qq.com>
2025-02-09 17:54:30 +0800

    '20250207_13'

--- pick/views.py
+++ pick/views.py
@@ -1555,7 +1555,7 @@ def block_single(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="QFII重仓"
+    blockname ="QFII新进"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
