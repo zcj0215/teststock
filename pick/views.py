aa914e32fc275738afb38f8b02a11f96ae442a00
赵从军 <702065725@qq.com>
2025-09-14 15:40:45 +0800

    '20250909_4'

--- pick/views.py
+++ pick/views.py
@@ -1756,7 +1756,7 @@ def block_weihu(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="北上重仓"
+    blockname ="大基金持股"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
