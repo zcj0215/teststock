bad85d24904dac1457bca7439f1a35db80dc45d2
赵从军 <702065725@qq.com>
2025-10-22 08:39:14 +0800

    '20251010_5'

--- pick/views.py
+++ pick/views.py
@@ -1756,7 +1756,7 @@ def block_weihu(request):
 def blockadd(request):
     path =  os.path.dirname(__file__)
     filename = "" 
-    blockname ="低空经济"
+    blockname ="CPO概念"
     if(sysstr =="Windows"):
         filename = path+"\\"+blockname+".xls"
     else:
